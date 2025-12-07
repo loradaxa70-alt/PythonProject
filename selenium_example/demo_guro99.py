040be2e274e30c5bd90a436d0b4a7041f962e337
lora <loradaxa70@gmail.com>
2025-12-07 14:58:56 +0200

    first_commit

---
+++ selenium_example/demo_guro99.py
@@ -0,0 +1,28 @@
+import time
+from time import sleep
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
+
+print("Test start")
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+
+driver.get ('https://demo.guru99.com/test/newtours/index.php')
+
+user= driver.find_element(By.ID,'userName')
+user.click()
+user.clear()
+user.send_keys('Lora')
+user.send_keys(Keys.ENTER)
+
+
+driver.close()
+
