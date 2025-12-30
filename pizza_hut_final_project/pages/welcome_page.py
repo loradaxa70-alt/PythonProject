6032920b9948f44a4efcb0151e2b9aaa2ddb3062
lora <loradaxa70@gmail.com>
2025-12-30 10:32:01 +0200

    global

--- pizza_hut_final_project/pages/welcome_page.py
+++ pizza_hut_final_project/pages/welcome_page.py
@@ -1,19 +1,24 @@
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
+from pizza_hut_final_project.locators import WelcomePageLocators
+
 
 class WelcomePage():
     def __init__(self, driver):
         self.driver = driver
 
     def search_for_product(self, product):
-        search = self.driver.find_element(By.ID, "search")
+        search = self.driver.find_element(*WelcomePageLocators.SEARCH)
         search.clear()
         search.send_keys(product)
         search.send_keys(Keys.ENTER)
 
+
     def returning_to_welcome_page(self):
-        self.driver.find_element(By.CLASS_NAME, "css-2b4rnz").click()
+        self.driver.find_element(WelcomePageLocators.RETURN_TO_WELCOME_PAGE).click()
+
+
 
     def click_button_and_get_text(self, text):
         button = self.driver.find_element(By.LINK_TEXT, text)
